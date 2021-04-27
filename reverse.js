876288f3262414a96577977b1f104021c3b52a27
Matt <mattluo604@hotmail.com>
2021-04-27 18:52:17 +0000

    instructor version of sum function

---
+++ reverse.js
@@ -0,0 +1,8 @@
+const args = process.argv;
+for (let i = 2; i < args.length; i++) {
+  let result = '';
+  for (let a = args[i].length - 1; a > -1; a--) {
+    result += args[i][a];
+  }
+  console.log(result);
+}
