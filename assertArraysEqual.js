66d2c66ddda12290876784ca65f1f3217f4c3c28
Matt <mattluo604@hotmail.com>
2021-04-28 03:19:03 +0000

    fixed assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,9 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let y = 0; y < array1.length; y++) {
-      if (array1[y] !== array2[y]) {
-        console.log("This two arrays are not the same.");
-      }
-    }
-    console.log("This two arrays are the same.");
+const assertArraysEqual = function (array3, array4) {
+  if (eqArrays(array3, array4)) {
+    console.log("This two arrays are not the same.");
   } else {
     console.log("This two arrays are not the same.");
   }
 };
-console.log(eqArrays([1, 2, 3],[1, 2, 3]));
+
+console.log(assertArraysEqual());
