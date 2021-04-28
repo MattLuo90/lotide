b179ccc2168453874c06988278ea6a3685282f84
Matt <mattluo604@hotmail.com>
2021-04-28 21:34:00 +0000

    new assertArrayEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = function (array3, array4) {
   if (eqArrays(array3, array4)) {
-    console.log("This two arrays are not the same.");
+    console.log("This two arrays are the same.");
   } else {
     console.log("This two arrays are not the same.");
   }
