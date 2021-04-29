3245350d23d01950ed9b847cc1ef259063332234
Matt <mattluo604@hotmail.com>
2021-04-29 23:46:10 +0000

    take until function

--- takeUntil.js
+++ takeUntil.js
@@ -18,5 +18,3 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
-
-
