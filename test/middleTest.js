34f5fbc21f849885f6addcba9508657ee18cccad
Matt <mattluo604@hotmail.com>
2021-05-03 22:00:42 +0000

    using mocha to check

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,7 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
-
-console.log(middle([1, 2, 3, 4]));
-
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2,3]));
+describe("#middle", () => {
+  it("returns the same arrays", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
