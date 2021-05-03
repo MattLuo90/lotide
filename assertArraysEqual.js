f4b1314d2f5f504df5435700e7716657cf7ba38d
Matt <mattluo604@hotmail.com>
2021-05-03 21:35:05 +0000

    cleaned assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,9 @@
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = function (array3, array4) {
   if (eqArrays(array3, array4)) {
-    console.log("This two arrays are the same.");
+    return "This two arrays are the same.";
   } else {
-    console.log("This two arrays are not the same.");
+    return "This two arrays are not the same.";
   }
 };
 
