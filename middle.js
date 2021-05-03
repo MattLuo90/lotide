f35dbf8a56d1de230f08028b4d8b422c67b54424
Matt <mattluo604@hotmail.com>
2021-05-03 20:46:26 +0000

    cleaned middle function

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (y = 0; y < array1.length; y++) {
-      if (array1[y] !== array2[y]) {
-        return false;
-      }
-    }
-    return true;
-  } else { 
-    return false; 
-  }
-}
-
-const assertArraysEqual = function (array3, array4) {
-  if (eqArrays(array3, array4)) {
-    console.log("This two arrays are the same.");
-  } else {
-    console.log("This two arrays are not the same.");
-  }
-};
-
 const middle = function (array) {
   if (array.length > 2) {
     let newArray = [];
@@ -33,6 +12,5 @@ const middle = function (array) {
     return newArray;
   }
 }
-console.log(middle([1, 2, 3, 4]));
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2,3]));
+module.exports = middle;
